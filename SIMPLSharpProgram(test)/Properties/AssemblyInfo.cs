23aab552998f01307822e34bf517e250ee0b10a3
razaman2 <razamvmt@gmail.com>
2017-05-07 20:44:02 -0400

    Crestron Simpl# Project
    First simpl# project, this in a new project with boilerplate code.

---
+++ SIMPLSharpProgram(test)/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("SIMPLSharpProgram_test_")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SIMPLSharpProgram_test_")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyVersion("1.0.0.*")]
+
